77f274b5b82bf5dec60cad17ba92773558a9666a
adithyaGowda <adithyagowda5696@gmail.com>
2019-09-11 15:00:31 +0530

    modified ih and sp profile services

--- service-provider-profile-service/src/main/java/com/stackroute/config/ServiceProviderRabbitMq.java
+++ service-provider-profile-service/src/main/java/com/stackroute/config/ServiceProviderRabbitMq.java
@@ -14,29 +14,29 @@ import org.springframework.context.annotation.Configuration;
 public class ServiceProviderRabbitMq {
 
     /*-----------------registration------------------------------ */
-    @Value("${spRegister.rabbitmq.queue}")
-    String queueName;
-
-    @Value("${spRegister.rabbitmq.exchange}")
-    String exchange;
-
-    @Value("${spRegister.rabbitmq.routingkey}")
-    String routingkey;
-
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, true);
-    }
-
-    @Bean
-    TopicExchange exchange() {
-        return new TopicExchange(exchange);
-    }
-
-    @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(routingkey);
-    }
+//    @Value("${spRegister.rabbitmq.queue}")
+//    String queueName;
+//
+//    @Value("${spRegister.rabbitmq.exchange}")
+//    String exchange;
+//
+//    @Value("${spRegister.rabbitmq.routingkey}")
+//    String routingkey;
+//
+//    @Bean
+//    Queue queue() {
+//        return new Queue(queueName, true);
+//    }
+//
+//    @Bean
+//    TopicExchange exchange() {
+//        return new TopicExchange(exchange);
+//    }
+//
+//    @Bean
+//    Binding binding(Queue queue, TopicExchange exchange) {
+//        return BindingBuilder.bind(queue).to(exchange).with(routingkey);
+//    }
 
 
     /* -----------------profile------------------- */
