1f7c512e172599bc075c7205706227ae22e1980a
mansi123-9161 <mansichourasia100@gmail.com>
2019-09-18 17:44:05 +0530

    updated task

--- recommended-query-service/src/main/java/com/stackroute/recommendedqueryservice/domain/ServiceProvider.java
+++ recommended-query-service/src/main/java/com/stackroute/recommendedqueryservice/domain/ServiceProvider.java
@@ -1,39 +1,42 @@
-package com.stackroute.recommendedqueryservice.domain;
+package com.stackroute.squad.domain;
+
 
 import lombok.*;
+import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
 import java.util.List;
 
+/*@NodeEntity will be represented as nodes in the graph.*/
 @NodeEntity
-/**With @Data, Lombok will generate getter and setter methods, toString methods, Equal & Hashcode methods*/
+/*it can generate getters and setters for those object automatically by using Lombok annotations. The easiest way is to use the @Data annotation.*/
 @Data
-
-/**@AllArgsConstructor will generate constructor with all properties in the class*/
-@AllArgsConstructor
-
-/**@NoArgsConstructor will generate constructor with no arguments*/
+/*It generates constructor with no parameters*/
 @NoArgsConstructor
+/*It generates a cnstructor with 1 parameter for each field*/
+@AllArgsConstructor
 @Getter
 @Setter
 @ToString
 public class ServiceProvider {
-    /**
-     * Id annotation makes id variable as Primary key
-     */
-    @Id
-    private Long id;
-    private String name;
-    private String mobileNo;
-    private String email;
-    private String domain;
-    private String subDomain;
-    private String previousProject;
-    private String chargePerHour;
-    private String currentLocation;
-     private List<String> preferredLocation;
-    @Relationship(type = "has_skills", direction = Relationship.OUTGOING)
-    private List<Skills> skills;
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String name;
+  private long mobileNo;
+  private String email;
+  private String domain;
+  private String subDomain;
+  private String previousProject;
+  private String chargePerHour;
+  private String currentLocation;
+  private List<String> preferredLocation;
+  @Relationship(type = "has_skills", direction = Relationship.OUTGOING)
+  private List<Skills> skills;
+  @Relationship(type = "worked_on", direction = Relationship.OUTGOING)
+  private Idea idea;
+  @Relationship(type = "applied_for", direction = Relationship.OUTGOING)
+  private Idea ideaDto;
 }
