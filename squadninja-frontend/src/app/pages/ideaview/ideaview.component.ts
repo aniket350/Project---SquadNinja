9e1b291a7aacf0b91428393cc832a3f9afa88442
manish.perumalla <manish.perumalla@cgi.com>
2019-09-22 17:14:02 +0530

    Update ideaview.component.ts

--- squadninja-frontend/src/app/pages/ideaview/ideaview.component.ts
+++ squadninja-frontend/src/app/pages/ideaview/ideaview.component.ts
@@ -49,7 +49,7 @@ export class IdeaviewComponent implements OnInit {
     console.log(email,this.title);
     this.ideaviewService.remove(this.title,email).subscribe((data) =>{
     });
-    window.location.reload();
+    //window.location.reload();
   }
   save(email){
     console.log(email);
@@ -57,14 +57,14 @@ export class IdeaviewComponent implements OnInit {
     console.log(status);
     this.ideaviewService.updateOnAccept(this.title,email,this.status).subscribe((data) =>{
     });
-    window.location.reload();
+   // window.location.reload();
   }
   reject(email){
      this.status=false;
      console.log(email,this.status,this.title);
      this.ideaviewService.updateOnReject(this.title,email,this.status).subscribe((data) =>{
   });
-  window.location.reload();
+  //window.location.reload();
 }
 
   join(email) {
