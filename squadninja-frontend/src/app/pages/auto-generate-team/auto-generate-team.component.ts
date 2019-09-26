08847acf9b692e507a708d71a3f49b478edfc88f
adithyaGowda <adithyagowda5696@gmail.com>
2019-09-26 21:06:29 +0530

    modified code

--- squadninja-frontend/src/app/pages/auto-generate-team/auto-generate-team.component.ts
+++ squadninja-frontend/src/app/pages/auto-generate-team/auto-generate-team.component.ts
@@ -31,7 +31,7 @@ export class AutoGenerateTeamComponent implements OnInit {
     }
 }
   toSendData = new Map();
-  constructor(private autogenerate : AutogenerateService, private http:HttpClient,private autogeneratesp:AutogenerateService) { }
+  constructor( private http:HttpClient,private autogeneratesp:AutogenerateService) { }
   ngOnInit() {
      let  disp=[];
 // this.getPostedIdeas(); 
